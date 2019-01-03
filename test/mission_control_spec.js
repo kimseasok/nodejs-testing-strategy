db08c52c5e3bf56bc5066854c5fab57e2f6a18d9
kimseasok <basicblogtalk@gmail.com>
2019-01-03 04:44:58 -0500

    Subbling with Sinon

---
+++ test/mission_control_spec.js
@@ -0,0 +1,26 @@
+var mement = require('moment');
+var MissionControl = require('../models/mission_control');
+var assert = require('assert');
+var db = require('../db');
+var sinon = require('sinon');
+
+sinon.stub(db, 'find').yields(null, { id: 1 });
+
+var missionControl = new MissionControl({ db: db });
+
+describe('Mission Control', function () {
+    describe('The current mission', function () {
+        var currentMission;
+        before(function (done) {
+            missionControl.currentMission(function (err, res) {
+                currentMission = res;
+                done();
+            });
+
+        });
+
+        it('is created if none exists', function () {
+            assert(currentMission);
+        })
+    });
+});
